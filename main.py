5384deddded8526edad28cd5fe3867d6283c4013
mohamed77770 <m.m.reda.2200@gmail.com>
2026-08-04 23:06:22 +0300

    read campaigns endpoint

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(root_path="/api/v1")
 
 @app.get("/")
 async def root():
     return {"message": "Hello"}
+
+
+@app.get("/campaigns")
+async def read_campaigns():
+    return {"campaigns": "example"}
