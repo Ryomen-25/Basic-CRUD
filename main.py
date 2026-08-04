bbd52083c6468117745f4e40a5ce8e3eff48058e
mohamed77770 <m.m.reda.2200@gmail.com>
2026-08-04 23:23:26 +0300

    get id endpoint

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from datetime import datetime
+from typing import Any
 
 app = FastAPI(root_path="/api/v1")
 
@@ -6,7 +8,29 @@ app = FastAPI(root_path="/api/v1")
 async def root():
     return {"message": "Hello"}
 
+data : Any = [
+    {
+        "campaign_id": 1,
+        "name": "Summer Training",
+        "due_date": datetime.now(),
+        "created_at": datetime.now()
+    },     
+    {
+        "campaign_id": 2,
+        "name": "Summer Internship",
+        "due_date": datetime.now(),
+        "created_at": datetime.now()
+    }
+]
 
 @app.get("/campaigns")
 async def read_campaigns():
-    return {"campaigns": "example"}
+    return {"campaigns": data}
+
+
+@app.get("/campaigns/{id}")
+async def read_campaign(id: int):
+    for campaign in data:
+        if campaign.get("campaign_id") == id:
+            return {"campaign": id}
+    raise HTTPException(status_code=404)
